b57548e398570e3d56da7cefc5dd804c00dabcdb
kruserr <46799551+kruserr@users.noreply.github.com>
2024-10-23 11:05:11 +0000

    fix: add simple cli main assetr_cmd smoke test

--- pakka/src/main.rs
+++ pakka/src/main.rs
@@ -3,3 +3,14 @@ use pakka;
 fn main() {
   pakka::cli_main();
 }
+
+#[test]
+fn cli_main_smoke_test() {
+  let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+  cmd
+    .arg("--help")
+    .assert()
+    .success()
+    .stdout(predicates::str::contains(env!("CARGO_PKG_NAME")));
+}
